b05543348cf6091615c7e7ffc89e67d19cc3220c
KirtikaKhowal <kanishkakhowal21@gmail.com>
2026-01-28 15:42:04 +0530

    js day

--- JS day-5/script.js
+++ JS day-5/script.js
@@ -5,3 +5,7 @@ console.log(headingElement)
 headingElement.textContent="New heading";
 headingElement.style.color="blue";
 headingElement.style.backgroundColor="yellow";
+
+const para= document.querySelector("#content p")  
+        console.log(para);
+        para.style.color="orange";
