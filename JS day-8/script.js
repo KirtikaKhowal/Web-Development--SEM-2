1dc6757cb4ccee0ddb2abb1fb79ec42628c5d306
KirtikaKhowal <kanishkakhowal21@gmail.com>
2026-02-11 19:57:36 +0530

    assignment

---
+++ JS day-8/script.js
@@ -0,0 +1,16 @@
+function handleClick(){
+    document.getElementById("btn").textContent="Clicked"
+}
+
+//Que 1:-add a h1 tag and button and when user click on the button the colour of h1 will change
+function handleClick(){
+    document.getElementById("btn2").color=""
+}
+
+
+
+//Q2:-create a button and when user click on the button on h2 tag with text and image will render below o the button
+const addText=()=>{
+    document.createElement("h2").textContent;
+
+}
--- TASK-1/script.js
+++ TASK-1/script.js
@@ -4,14 +4,14 @@ const profile = document.getElementById("profile");
 profile.style.backgroundColor=("pink");
 profile.style.padding=("15px");
 profile.style.textAlign=("center")
-//----------------------------------------------------
+//-------------------------------------------------
 //Task-2
-//---------------------------------------------------------------
+//-------------------------------------------------
 const highlight = document.getElementsByClassName("important");
 console.log(len(highlight));
-
+console.log(highlight)
 for(let i=0;i<highlight.length;i++){
     highlight[i].style.color=("red");
     highlight[i].style.fontSize=("200px");
 }
-//---------------------------------------------------------------
+//---------------------------------------------------
